3b2aa651c81a1f180a2730df0e3af92c11d88bfa
Alexander Myasoedov <msoedov@gmail.com>
2023-05-10 17:49:25 +0300

    Revert "fix(test deployment):"
    This reverts commit 52b8026e658c270543d863e905684df66eb0f323.

--- examples/app.py
+++ examples/app.py
@@ -4,8 +4,8 @@ from langcorn import create_service
 app: FastAPI = create_service(
     "examples.ex1:chain",
     "examples.ex2:chain",
-    # "examples.ex3:chain",
-    # "examples.ex4:sequential_chain",
-    # "examples.ex5:conversation",
-    # "examples.ex6:conversation_with_summary",
+    "examples.ex3:chain",
+    "examples.ex4:sequential_chain",
+    "examples.ex5:conversation",
+    "examples.ex6:conversation_with_summary",
 )
