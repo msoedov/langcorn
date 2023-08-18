69b8e8d2de1c5d63c496ffd1bd0a302e32e94abf
Alexander Miasoiedov (Myasoedov) <msoedov@gmail.com>
2023-08-18 16:07:53 +0300

    fix(dev build) (#29)
    * feat(test deployment):
    
    * feat(simplify example):
    
    * feat(refactor vercel app):
    
    * fix(dev build):
    
    * fix(vercel json):

---
+++ examples/vercel.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from langcorn import create_service
+
+app: FastAPI = create_service(
+    "examples.ex1:chain",
+    "examples.ex2:chain",
+    "examples.ex3:chain",
+    "examples.ex4:sequential_chain",
+    "examples.ex5:conversation",
+    "examples.ex6:conversation_with_summary",
+    "examples.ex7_agent:agent",
+    "examples.ex9_double_chain:chain1",
+    "examples.ex9_double_chain:chain2",
+    "examples.ex10_cc:chain",
+)
